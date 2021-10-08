cee325c0206642c61631ae112d145b1e79b2e6e4
Felype Henrique <felypehenrique25@gmail.com>
2021-10-08 10:23:57 -0300

    formulário

--- src/Components/pages/NewProject.js
+++ src/Components/pages/NewProject.js
@@ -1,5 +1,16 @@
-function NemProject (){
-    return <h1>NemProject</h1>
+import React from "react"
+import ProjectForm from "../project/ProjectForm"
+
+import styles from './NewProject.module.css'
+
+function NewProject (){
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
+    )
 }
 
-export default NemProject
+export default NewProject
