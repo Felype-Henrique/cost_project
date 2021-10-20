43764c6536e75dfba0ff91a93705603d3b756f25
Felype Henrique <felypehenrique25@gmail.com>
2021-10-20 10:38:24 -0300

    area de serviço

--- src/Components/pages/Project.js
+++ src/Components/pages/Project.js
@@ -14,6 +14,7 @@ function Project(){
     
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -35,6 +36,8 @@ function Project(){
     },[id])
 
     function editPost(project) {
+        setMessage('')
+
         //budget validation
         if(project.budget < project.cost){
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
@@ -64,6 +67,10 @@ function Project(){
 
     function toggleProjectForm(){
         setShowProjectForm (!showProjectForm)
+    }
+
+    function toggleServiceForm(){
+        setShowServiceForm (!showServiceForm)
     }
 
     return (
@@ -98,6 +105,19 @@ function Project(){
                     </div>
                 )}
             </div>
+            <div className={styles.service_form_container}>
+                <h2>Adicione um serviço</h2>
+                <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? 'Adicionar serviço' : 'Fechar' }
+                </button>
+            <div className={styles.project_info}>
+                {showServiceForm && <div> formulário do serviço</div>}
+            </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass="start">
+                <p>Itens de serviços</p>
+            </Container>
             </Container>
         </div>
         ):(
